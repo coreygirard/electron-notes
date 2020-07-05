9bccd4ce03545a024f09649725d989906cf6c506
Corey Girard <corey.r.girard@gmail.com>
2020-07-05 19:55:41 +1200

    Basics working

--- renderer.js
+++ renderer.js
@@ -4,3 +4,43 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+$("#myfirstview").hide();
+$("#mysecondview").hide();
+
+// on click show the second view
+$("#showall").on("click", function() {
+  $("#mysecondview").show(); // show the second view
+  $("#myfirstview").hide(); // hide the first view
+  return false;
+});
+
+$("#save-form").on("click", function() {
+  console.log($("#email").val() + " " + $("#contact").val()); // getting input values
+  return false;
+});
+
+const inputHandler = function(e) {
+  console.log(e.target.value);
+};
+
+//const { dialog } = require("electron").remote;
+
+//document.getElementById("email").addEventListener("input", inputHandler);
+
+$("#title").on("input", function(e) {
+  //var filename = $('#filename').val(); // get the filename
+  //var filecontent = $('#fileconent').val(); // get the file contents
+
+  var filename = "something.txt";
+  var filecontent = e.target.value;
+
+  // write the filecontent to the selected file
+  document.fs.writeFile(filename, filecontent, function(err) {
+    if (err) {
+      //return console.log(err);
+    }
+    return true;
+  });
+  return false;
+});
