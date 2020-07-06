3d4d4d8edd07220b85b0f0f6545b09c0c0e0c6cd
Corey Girard <corey.r.girard@gmail.com>
2020-07-06 12:34:03 +1200

    Rough CRUD functionality

--- renderer.js
+++ renderer.js
@@ -5,35 +5,62 @@
 // selectively enable features needed in the rendering
 // process.
 
-$("#myfirstview").hide();
-$("#mysecondview").hide();
+document.fs.readdir("./notes/", populateExisting);
 
-// on click show the second view
-$("#showall").on("click", function() {
-  $("#mysecondview").show(); // show the second view
-  $("#myfirstview").hide(); // hide the first view
+$("#edit-page").hide();
+$("#browse").on("click", browseFiles);
+
+function browseFiles() {
+  document.fs.readdir("./notes/", populateExisting);
+  $("#browse-page").show();
+  $("#edit-page").hide();
   return false;
-});
+}
 
-$("#save-form").on("click", function() {
-  console.log($("#email").val() + " " + $("#contact").val()); // getting input values
+$("#makenew").on("click", editNew);
+
+$("#delete").on("click", deleteNote);
+
+function deleteNote(e) {
+  let filename = document.getElementById("filename").innerHTML;
+  let path = "./notes/" + filename;
+  console.log("deleting: " + path);
+  document.fs.unlinkSync(path);
+  browseFiles();
   return false;
-});
+}
 
-const inputHandler = function(e) {
-  console.log(e.target.value);
-};
+// https://gist.github.com/kevinbull/f1cbc5440aa713bd5c9e
+function generateUnid(a) {
+  return a
+    ? (a ^ ((Math.random() * 16) >> (a / 4))).toString(16)
+    : ([1e10] + 1e10 + 1e9).replace(/[01]/g, generateUnid).toUpperCase();
+}
 
-//const { dialog } = require("electron").remote;
+function editNew() {
+  document.getElementById("filename").innerHTML = generateUnid() + ".json";
+  document.getElementById("title").value = "";
+  document.getElementById("text").value = "";
+  $("#browse-page").hide();
+  $("#edit-page").show();
+  return false;
+}
 
-//document.getElementById("email").addEventListener("input", inputHandler);
+function editExisting(e) {
+  document.getElementById("filename").innerHTML = e.target.fileName;
+  document.getElementById("title").value = e.target.fileData.title;
+  document.getElementById("text").value = e.target.fileData.text;
+  $("#browse-page").hide();
+  $("#edit-page").show();
+  return false;
+}
 
-$("#title").on("input", function(e) {
-  //var filename = $('#filename').val(); // get the filename
-  //var filecontent = $('#fileconent').val(); // get the file contents
-
-  var filename = "something.txt";
-  var filecontent = e.target.value;
+function writeFile() {
+  var filename = "./notes/" + document.getElementById("filename").innerHTML;
+  var filecontent = JSON.stringify({
+    title: document.getElementById("title").value,
+    text: document.getElementById("text").value
+  });
 
   // write the filecontent to the selected file
   document.fs.writeFile(filename, filecontent, function(err) {
@@ -43,4 +70,31 @@ $("#title").on("input", function(e) {
     return true;
   });
   return false;
-});
+}
+
+$("#title").on("input", writeFile);
+$("#text").on("input", writeFile);
+
+function populateExisting(err, files) {
+  let parent = document.getElementById("existing");
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+
+  for (const s of files) {
+    let filedata = JSON.parse(document.fs.readFileSync("./notes/" + s));
+    console.log(filedata);
+
+    let but = document.createElement("button");
+    but.innerHTML = filedata.title;
+    but.className = "edit-existing";
+    but.fileName = s;
+    but.fileData = filedata;
+
+    but.onclick = editExisting;
+    document.getElementById("existing").appendChild(but);
+    document
+      .getElementById("existing")
+      .appendChild(document.createElement("br"));
+  }
+}
